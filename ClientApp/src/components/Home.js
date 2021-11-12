8cf1c8f099eabed048c20f7f1c1c529d95d23cd0
guttesentheodor <guttesentheodor@gmail.com>
2021-11-12 15:53:24 +0100

    added material and emotion react

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Placeholder } from './Placeholder';
+import ProdComp from './customization_table/ProdComp';
+
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -10,6 +12,7 @@ export class Home extends Component {
         <h1>Project Customize Products</h1>
             <a href='https://docs.google.com/document/d/1nOoYAFE4BTC2kovLBC13Mlza9_vf0f1Z4owRk-vuGK8/edit?usp=sharing'>Project Report in Google docs</a>
             <Placeholder></Placeholder>
+            <ProdComp></ProdComp>
       </div>
     );
   }
